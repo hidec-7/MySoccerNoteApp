1fde23f96f8790e1ca6541c4c33f563608aa8097
hidec-7 <hiedematoaho0730127@gmail.com>
2020-11-05 17:38:26 +0900

    UITableView実装に必要なコード追記

--- SoccerNoteApp/GameManagementViewController.swift
+++ SoccerNoteApp/GameManagementViewController.swift
@@ -9,13 +9,40 @@ import UIKit
 
 class GameManagementViewController: UIViewController {
 
+    private let cellId = "cellId"
+    
     @IBOutlet weak var gameManagementTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gameManagementTableView.delegate = self
+        gameManagementTableView.dataSource = self
 
     }
 
+}
+
+extension GameManagementViewController: UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = gameManagementTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        return cell
+        
+    }
+    
+    
+    
+    
 }
 
 class GameManagementTableViewCell: UITableViewCell {
