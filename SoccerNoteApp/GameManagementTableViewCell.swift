61c31b29abdffb0eb428f1cbc525d2f3ebaed8ab
hidec-7 <hiedematoaho0730127@gmail.com>
2020-11-07 14:24:23 +0900

    GameManagementTableViewCellにコード追記

--- SoccerNoteApp/GameManagementTableViewCell.swift
+++ SoccerNoteApp/GameManagementTableViewCell.swift
@@ -5,4 +5,17 @@
 //  Created by hideto c. on 2020/11/07.
 //
 
-import Foundation
+import UIKit
+
+class GameManagementTableViewCell: UITableViewCell {
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
