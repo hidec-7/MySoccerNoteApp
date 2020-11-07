a728900e20411e537ff3bfc5294a787850fb1b18
hidec-7 <hiedematoaho0730127@gmail.com>
2020-11-07 13:31:10 +0900

    GameManagementViewControllerファイルを追加

---
+++ SoccerNoteApp/GameManagementViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameManagementViewController.swift
+//  SoccerNoteApp
+//
+//  Created by hideto c. on 2020/11/07.
+//
+
+import UIKit
+
+class GameManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
