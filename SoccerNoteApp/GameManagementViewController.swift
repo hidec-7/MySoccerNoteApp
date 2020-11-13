b3cf2799e90afd8bfae19950fba77221c697b415
hidec-7 <hiedematoaho0730127@gmail.com>
2020-11-14 00:17:27 +0900

    余計なスペース修正

--- SoccerNoteApp/GameManagementViewController.swift
+++ SoccerNoteApp/GameManagementViewController.swift
@@ -15,7 +15,6 @@ class GameManagementViewController: UIViewController {
     @IBOutlet weak var gameManagementTableView: UITableView!
     @IBOutlet weak var gameAddButton: UIBarButtonItem!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
