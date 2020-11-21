25f91334e69e4bc145a04a0852c34c04e0b82745
hidec-7 <hiedematoaho0730127@gmail.com>
2020-11-22 02:51:30 +0900

    Labelとコードを紐付け

--- SoccerNoteApp/GameManagementTableViewCell.swift
+++ SoccerNoteApp/GameManagementTableViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class GameManagementTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var opponentTeamLabel: UILabel!
+    @IBOutlet weak var myScoreLabel: UILabel!
+    @IBOutlet weak var opponentScoreLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
