10475683316a30043f6e85fba24a61a7ddee2424
hidec-7 <hiedematoaho0730127@gmail.com>
2020-11-26 17:10:08 +0900

    余計なスペース修正

--- SoccerNoteApp/GameRegisterViewController.swift
+++ SoccerNoteApp/GameRegisterViewController.swift
@@ -30,7 +30,6 @@ class GameRegisterViewController: UIViewController,UITextFieldDelegate,UINavigat
         opponentScoreTextField.delegate = self
         
         setupKeyboard()
-        
         setupTextView()
     }
     
