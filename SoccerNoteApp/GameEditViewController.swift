e9daa8160b59fe77d50ae2d9fc630c3172fed9b5
hidec-7 <hiedematoaho0730127@gmail.com>
2020-12-04 17:51:05 +0900

    NavigationBar実装

--- SoccerNoteApp/GameEditViewController.swift
+++ SoccerNoteApp/GameEditViewController.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 
-class GameEditViewController: UIViewController {
-
+class GameEditViewController: UIViewController,UINavigationBarDelegate {
+    
+    @IBOutlet weak var gameEditNavigationBar: UINavigationBar!
+    
+    @IBAction func didTapBackButton(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        gameEditNavigationBar.delegate = self
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
     }
 
 }
