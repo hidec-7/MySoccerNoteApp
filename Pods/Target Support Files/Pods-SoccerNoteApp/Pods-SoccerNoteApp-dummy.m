213ab6131cb22ab3de94a57b1d29c8888bd7e7eb
hidec-7 <hiedematoaho0730127@gmail.com>
2020-12-30 14:17:39 +0900

    キャッシュを削除

---
+++ Pods/Target Support Files/Pods-SoccerNoteApp/Pods-SoccerNoteApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SoccerNoteApp : NSObject
+@end
+@implementation PodsDummy_Pods_SoccerNoteApp
+@end
---
+++ SoccerNoteApp/View/GameManagementTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  GameManagementTableViewCell.swift
+//  SoccerNoteApp
+//
+//  Created by hideto c. on 2020/11/07.
+//
+
+import UIKit
+
+class GameManagementTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var opponentTeamLabel: UILabel!
+    @IBOutlet weak var myScoreLabel: UILabel!
+    @IBOutlet weak var opponentScoreLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
