ddfda77ef3dda04877b717492733b73ab9010f92
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-01-28 19:33:50 +0900

    不要な箇所を削除

--- SoccerNoteApp/Controller/GameManagementViewController.swift
+++ SoccerNoteApp/Controller/GameManagementViewController.swift
@@ -32,30 +32,6 @@ class GameManagementViewController: UIViewController {
         GameDataReadModel.getGameData(table: gameManagementTableView)
     }
     
-//    private func getGameData(table: UITableView) {
-//        let ref = Database.database().reference()
-//        guard let uid = Auth.auth().currentUser?.uid else { return }
-//        
-//        GameDataModel.gameDataListArray.removeAll()
-//        
-//        ref.child(uid).observeSingleEvent(of: .value) { (snapshot) in
-//            for data in snapshot.children {
-//                let snapData = data as! DataSnapshot
-//                let dictionarySnapData = snapData.value as! [String: Any]
-//                let gameDate = GameDataModel(gameDate: dictionarySnapData["gameDate"] as! String,
-//                                             team: dictionarySnapData["team"] as! String,
-//                                             myScore: dictionarySnapData["myScore"] as! String,
-//                                             opponentScore: dictionarySnapData["opponentScore"] as! String,
-//                                             firstHalf: dictionarySnapData["firstHalf"] as? String ?? "",
-//                                             secondHalf: dictionarySnapData["secondHalf"] as? String ?? "",
-//                                             conclusion: dictionarySnapData["conclusion"] as? String ?? "")
-//                GameDataModel.gameDataListArray.append(gameDate)
-//            }
-//            GameDataModel.gameDataListArray.reverse()
-//            table.reloadData()
-//        }
-//    }
-    
     private func headerTitle() {
         title = "試合管理"
     }
