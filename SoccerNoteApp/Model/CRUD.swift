daa4738e528a844ac01b85740bdabae00047851c
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-01-28 19:39:59 +0900

    さらに余計なファイルを削除

--- SoccerNoteApp/Model/CRUD.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  SoccerNoteApp
-//
-//  Created by 渕一真 on 2021/01/28.
-//
-
-import Foundation
--- SoccerNoteApp/Model/GameDateReadModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  GameDateReadModel.swift
-//  SoccerNoteApp
-//
-//  Created by 渕一真 on 2021/01/28.
-//
-
-import Foundation
