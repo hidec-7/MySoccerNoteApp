3e989b72c65307863df175e53b805296a258e06f
hidec-7 <hiedematoaho0730127@gmail.com>
2021-04-17 16:47:38 +0900

    メニュー画面UI実装

---
+++ SoccerNoteApp/Controller/SlideMenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SlideMenuViewController.swift
+//  SoccerNoteApp
+//
+//  Created by hideto c. on 2021/04/17.
+//
+
+import UIKit
+
+class SlideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
