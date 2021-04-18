75c105e0655efc1d6f21f2b2967558a2ac992cf8
hidec-7 <hiedematoaho0730127@gmail.com>
2021-04-19 08:10:06 +0900

    スライドメニュー画面UI-UX実装

--- SoccerNoteApp/Controller/SlideMenuViewController.swift
+++ SoccerNoteApp/Controller/SlideMenuViewController.swift
@@ -9,8 +9,35 @@ import UIKit
 
 class SlideMenuViewController: UIViewController {
 
+    @IBOutlet private weak var slideMenuView: UIView!
+    @IBOutlet private weak var nonMenuView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupSlideMenu()
+    }
+
+    private func setupSlideMenu() {
+        let menuPosi = self.slideMenuView.layer.position
+        self.slideMenuView.layer.position.x = self.slideMenuView.frame.width
+
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut) {
+            self.slideMenuView.layer.position.x = menuPosi.x
+        } completion: { _ in }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        for touch in touches where touch.view == nonMenuView {
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn) {
+                self.slideMenuView.layer.position.x = -self.slideMenuView.frame.width
+            } completion: { _ in
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
     }
 }
